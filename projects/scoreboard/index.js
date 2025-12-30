4996ec9d44f9b8f3abbc4125971fa48caeabd531
Alex Hewitt <alexhewitt.j@gmail.com>
2025-12-30 09:29:33 -0600

    add finalized code

--- projects/scoreboard/index.js
+++ projects/scoreboard/index.js
@@ -7,12 +7,33 @@ let homeScoreBtns = document.getElementById("home-score-btns");
 let awayScoreBtns = document.getElementById("away-score-btns");
 
 let awayScore = document.getElementById("away-score");
+let homeScore = document.getElementById("home-score");
+let newGameBtn = document.getElementById("new-game");
 
 awayScoreBtns.addEventListener("click", (event) => {
   const clickedButton = event.target;
   const action = clickedButton.getAttribute("data-action");
   if (action === "add-one") {
-    console.log("hi");
     awayScore.textContent = Number(awayScore.textContent) + 1;
+  } else if (action === "add-two") {
+    awayScore.textContent = Number(awayScore.textContent) + 2;
+  } else if (action === "add-three") {
+    awayScore.textContent = Number(awayScore.textContent) + 3;
   }
+});
+homeScoreBtns.addEventListener("click", (event) => {
+  const clickedButton = event.target;
+  const action = clickedButton.getAttribute("data-action");
+  if (action === "add-one") {
+    homeScore.textContent = Number(homeScore.textContent) + 1;
+  } else if (action === "add-two") {
+    homeScore.textContent = Number(homeScore.textContent) + 2;
+  } else if (action === "add-three") {
+    homeScore.textContent = Number(homeScore.textContent) + 3;
+  }
+});
+
+newGameBtn.addEventListener("click", (event) => {
+  awayScore.textContent = "0";
+  homeScore.textContent = "0";
 });
